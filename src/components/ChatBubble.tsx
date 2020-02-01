72128a24867fd80d8943f30d7f3af16a7f96463c
Nick DeGroot <nick.degroot@streem.pro>
2020-01-31 23:34:07 -0800

    Example chat bubble storybook

---
+++ src/components/ChatBubble.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from 'react';
+
+const ChatBubble: FC = () => {
+    return <div />
+}
+
+export default ChatBubble;
---
+++ src/stories/chat.stories.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { withKnobs, text } from "@storybook/addon-knobs";
+import ChatBubble from "../components/ChatBubble";
+
+export const chatBubble = () => {
+    return (<ChatBubble user={text('message', 'Hello!')} />)
+}
+ 
+export default {
+  title: "Chat Components",
+  decorators: [withKnobs],
+};
