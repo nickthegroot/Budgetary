d9392941283ffd3cfa5125007c5f4a2862e063cc
Divyanshi Srivastava <divyanshi1350@gmail.com>
2020-01-31 23:46:55 -0800

    adding chat bubble

--- src/components/ChatBubble.tsx
+++ src/components/ChatBubble.tsx
@@ -1,7 +1,11 @@
 import React, { FC } from 'react';
 
-const ChatBubble: FC = () => {
-    return <div />
+const ChatBubble: FC = (props) => {
+    return (
+        <div className="chat-bubble-container" >
+            <div className="name">{props}</div>
+        </div>
+    )
 }
 
 export default ChatBubble;
