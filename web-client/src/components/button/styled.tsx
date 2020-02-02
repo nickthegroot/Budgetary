3a1e701200e8182669e2174461e421c6b41bb968
Nick DeGroot <nick.degroot@streem.pro>
2020-02-02 00:56:42 -0800

    More natural selection buttons

--- web-client/src/components/button/styled.tsx
+++ web-client/src/components/button/styled.tsx
@@ -11,29 +11,16 @@ const ButtonWrapper = styled.button<ButtonProps>`
   text-align: center;
   font-weight: 600;
   color: white;
-  width: 100%;
+  float: right;
   line-height: 30px;
   transition: all ease-in-out 0.2s;
-  color: black;
   border: 1px solid #e7ebf2;
-  background-color: white;
+  background-color: #409af8;
+  border-radius: 10px;
+  margin: 3px;
   &:hover {
-    background-color: #e7ebf2;
+    background-color: #3f86d1;
   }
-  ${props =>
-    props.color === "red" &&
-    css`
-    a {
-        color: white;
-    }
-    background-color: #8b0000 ;
-    border: 1px solid #6713c4;
-    box-shadow: 0 2px 0 #6713c4, 1px 3px 6px #6713c4;
-    color: white;
-    &:hover {
-        background-color: #6713c4;
-    }
-    `}
 `;
 
 export { ButtonWrapper };
