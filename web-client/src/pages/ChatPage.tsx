8fb01937bf1408e0f81745c63c21dbef7825c31b
Divyanshi Srivastava <divyanshi1350@gmail.com>
2020-02-01 18:33:34 -0800

    merge

--- web-client/src/pages/ChatPage.tsx
+++ web-client/src/pages/ChatPage.tsx
@@ -1,7 +1,14 @@
-import React, { FC } from 'react'
+import React, { FC } from 'react';
+import Bubble from "../components/Bubble/ChatBubble";
+import "./ChatPage.css";
 
 const ChatPage: FC = () => {
-    return <div />
+    return (
+    <div className="chat-page-container">
+        <div className="chat">
+            <Bubble />
+        </div>   
+    </div>)
 }
 
 export default ChatPage
